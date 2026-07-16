d6943ab509319a7048b0a310ec1fa13b640b2980
shailesh <shailesh.prajapati46@gmail.com>
2026-07-16 17:04:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello, world!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
