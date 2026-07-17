924b2b465eea3c617c97f63ea46fd159d37e6761
shailesh <shailesh.prajapati46@gmail.com>
2026-07-17 17:33:34 +0530

    changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello, world-6!' });
+  res.json({ message: 'Hello, world from Dockerhub!' });
 });
 
 app.listen(port, () => {
